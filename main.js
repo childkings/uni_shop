84c4130eeef7ed54fff658bdaf401b48de13cb10
peakTime <2441636874@qq.com>
2022-04-07 00:32:12 +0800

    完成了home页面的构建

--- main.js
+++ main.js
@@ -2,8 +2,26 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {$http} from '@escook/request-miniprogram'
 
-Vue.config.productionTip = false
+uni.$http = $http
+$http.baseUrl = 'https://api-ugo-web.itheima.net/api/public/v1'
+$http.beforeRequest = function (option) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+$http.afterRequest = function(option) {
+  if(option.data.meta.status !== 200) {
+    uni.showToast({
+      title: '数据请求失败！',
+      duration: 1500,
+      icon: 'none'
+    })
+  }
+  uni.hideLoading()
+}
+Vue.config.productionTip = true
 
 App.mpType = 'app'
 
